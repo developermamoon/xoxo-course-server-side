32e1cc73040dcd2b23eaf3dd116e56431ce6b0c0
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-10-28 10:38:36 +0600

    links created

--- index.js
+++ index.js
@@ -12,7 +12,30 @@ app.get('/', (req, res) => {
     res.send('XOXO Course API Running');
 });
 
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+});
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(courses);
+    }
+    else {
+        const category_course = courses.filter(n => n.category_id === id);
+        res.send(category_course);
+    }
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(n => n._id === id);
+    res.send(selectedCourse);
+});
 
 app.listen(port, () => {
     console.log('XOXO Course Server running on port', port);
